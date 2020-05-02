ff6cd71a4cbe3ce47ce7cf7feeca1ca0d28f9166
alexkiritsa <skiritsa@gmail.com>
2020-05-02 20:47:17 +0300

    Initial commit

---
+++ CatApiTest/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  CatApiTest
+//
+//  Created by Alex Kiritsa on 02.05.2020.
+//  Copyright © 2020 Alex Kiritsa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
